9b703eb23f499d3467753914497b45459d040fa6
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-06-14 18:44:45 +0530

    Hello world programming code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,13 @@
 public class HelloWorld {
-
+    /*public access modifier
+    class keyword is used to create class after that type class name in capital first letter
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        /*
+        this is our main method in which we have passed Strings as arguments
+        after that we are using System.out for ouput and we use System.in for taking input
+        after that we have println() used to print a new line we can use print() also to print but no new line will be printed
+         */
+    }
 }
