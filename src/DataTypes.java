cf256319423c9782f22b321b27152a19bdaeaca9
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-07-03 22:17:08 +0530

    Data types in java

---
+++ src/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a=127; //byte data type-stores number from -127 to 127
+        int b=1; //integer data type-stores integer values
+        float c=1.0f; //float data type - stores small decimal values know as floating point values
+        double d=1.536; //double data type - stores large decimal values
+        long e=1234567894; //long data type - stores large integer values
+        char f='c'; //character data type - stores characters put within 'inside this'
+        String s="Hello"; //string data type - stores string i.e. names etc put within "inside this"
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(s);
+    }
+}
